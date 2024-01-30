8faff753416d74f8e62daeeb7c6f2001cfb5f69d
Kavindi Chethika <126706751+K-Chethika@users.noreply.github.com>
2024-01-30 20:38:36 +0530

    Added Button group and scrollspy

--- dist/js/mz.js
+++ dist/js/mz.js
@@ -147,6 +147,36 @@ function remActive(){
     elemtnt.classList.remove(activeclasss);
     elemtnt.classList.add(className);
 }
+const dots = document.querySelectorAll(".scroll-indicator a");
 
+const removeActiveClass =() =>{
+    dots.forEach(dot =>{
+        dot.classList.remove("active");
+    });
+};
+
+const addActiveClass = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+           let currentDot = document.querySelector(`.scroll-indicator a[href='#${entry.target.id}']`);
+           removeActiveClass();
+           currentDot.classList.add("active");
+        }
+});
+
+};
+
+
+const options ={
+    threshold: 0.8,
+};
+
+const observer = new IntersectionObserver(addActiveClass, options);
+const sections = document.querySelectorAll("section");
+
+
+sections.forEach((section)=>{
+    observer.observe(section);
+});
 
 
